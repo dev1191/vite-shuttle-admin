958bfdbb9e7fd34167cc159914c4f6c883e3b3a8
Dev <devrajthapa1191@gmail.com>
2025-09-22 16:48:06 +0545

    add: language names

--- src/pages/[role]/profile.vue
+++ src/pages/[role]/profile.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/modules/user.store'
 
+const { t } = useI18n()
 const userStore = useUserStore()
 const userInfo = computed(() => userStore.user)
 </script>
@@ -15,15 +16,20 @@ meta:
 
 <template>
   <div>
-    <a-flex justify="space-between">
+    <a-flex gap="middle" justify="space-between">
       <a-card>
         <div class="text-lg font-semibold">User Profile</div>
       </a-card>
 
-      <a-card>
-        {{ userInfo }}
-        <a-button type="primary" @click="$router.back()">Back</a-button>
-      </a-card>
+      <div>
+        <a-card :title="t('user.userProfile')" :body-style="{ padding: 0, overflow: 'hidden' }">
+          {{ userInfo }}
+        </a-card>
+
+        <a-card>
+          <a-button type="primary" @click="$router.back()">Back</a-button>
+        </a-card>
+      </div>
     </a-flex>
   </div>
 </template>
